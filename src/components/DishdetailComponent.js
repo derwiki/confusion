ad7b2de92a159c9aa08758b027e2000144cf1227
Adam Derewecki <derewecki@gmail.com>
2020-03-30 20:52:42 -0700

    Switch to functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,68 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-    formatDate(date) {
-        // formats like Jan XX, XXXX
-        return new Date(date)
-                 .toLocaleDateString('en-EN', {weekday: undefined, month: 'short', day: 'numeric', year: 'numeric' })
-    }
+function formatDate(date) {
+    // formats like Jan XX, XXXX
+    return new Date(date)
+                .toLocaleDateString('en-EN', {weekday: undefined, month: 'short', day: 'numeric', year: 'numeric' })
+}
 
-    renderComments(comments) {
-        if (comments) {
-            const renderedComments = comments.map((comment) => {
-                return (
-                    <li key={comment.id}>
-                        {/* it's technically invalid to include <div> inside <li> (because they're both block elements), but it works */}
-                        <div>{comment.comment}</div>
-                        <div className='m-2'>
-                            — {comment.author}, {this.formatDate(comment.date)}
-                        </div>
-                    </li>
-                )
-            });
+function RenderDish({dish}) {
+    return (
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
 
+function RenderComments({comments}) {
+    if (comments) {
+        const renderedComments = comments.map((comment) => {
             return (
-                <div>
-                    <h4>Comments</h4>
-                    <ol className='list-unstyled'>
-                        {renderedComments}
-                    </ol>
-                </div>
+                <li key={comment.id}>
+                    {/* it's technically invalid to include <div> inside <li> (because they're both block elements), but it works */}
+                    <div>{comment.comment}</div>
+                    <div className='m-2'>
+                        — {comment.author}, {formatDate(comment.date)}
+                    </div>
+                </li>
             )
-        } else {
-            return (
-                <div />
-            )
-        }
-    }
-
-    render() {
-        const { dish } = this.props;
-        if (!dish) {
-            return null;
-        }
-        const { image, name, description, comments } = dish;
+        });
 
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={image} alt={name} />
-                            <CardBody>
-                                <CardTitle>{name}</CardTitle>
-                                <CardText>{description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(comments)}
-                    </div>
-                </div>
+            <div>
+                <h4>Comments</h4>
+                <ol className='list-unstyled'>
+                    {renderedComments}
+                </ol>
             </div>
+        )
+    } else {
+        return (
+            <div />
         )
     }
 }
 
+const DishDetail = ({ dish }) => {
+    if (!dish) {
+        return null;
+    }
+    const { comments } = dish;
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={comments} />
+                </div>
+            </div>
+        </div>
+    )
+}
 export default DishDetail;
